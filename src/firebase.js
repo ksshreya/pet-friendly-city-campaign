c88a62fcdb0aaaa2b998b6ad8fb6744a9f06ff19
ksshreya <138116845+ksshreya@users.noreply.github.com>
2025-10-07 18:59:28 +0530

    Add firebase initialization using env variables

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+// src/firebase.js
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import firebaseConfig from "./firebaseConfig";
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firestore
+const db = getFirestore(app);
+
+export { db };
